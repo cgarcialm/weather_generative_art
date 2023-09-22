98a82c52e0ac31920f908dbae47e24ddd2273d44
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2023-09-21 20:37:38 -0700

    Move json processors to own class

---
+++ src/JSONProcessor.java
@@ -0,0 +1,67 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JSONProcessor {
+    /**
+     * Reads JSON data from a file and returns it as a string.
+     *
+     * @param fileName The name of the JSON file.
+     * @return The JSON data as a string.
+     * @throws IOException If an I/O error occurs while reading the file.
+     */
+    public static String readJSONFromFile(String fileName) throws IOException {
+        StringBuilder jsonStr = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonStr.append(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return jsonStr.toString();
+    }
+
+    /**
+     * Parses JSON data into a list of WeatherData objects.
+     *
+     * @param jsonData The JSON data as a string.
+     * @return A list of WeatherData objects.
+     */
+    public static List<WeatherArtGenerator.WeatherData> parseJSONData(String jsonData) {
+        List<WeatherArtGenerator.WeatherData> weatherDataList = new ArrayList<>();
+
+        // Parse the JSON data
+        JSONObject jsonObject = new JSONObject(jsonData);
+
+        // Extract the "days" array
+        JSONArray daysArray = jsonObject.getJSONArray("days");
+
+        // Loop through each day's data
+        for (int i = 0; i < daysArray.length(); i++) {
+            JSONObject dayObject = daysArray.getJSONObject(i);
+
+            // Extract weather parameters for each date
+            float temperature = dayObject.getFloat("temp");
+            float windSpeed = dayObject.getFloat("windspeed");
+            float humidity = dayObject.getFloat("humidity");
+            float windDirection = dayObject.getFloat("winddir");
+
+            // Create a WeatherData object for the date and add it to the list
+            WeatherArtGenerator.WeatherData weatherData = new WeatherArtGenerator.WeatherData(temperature, windSpeed,
+                    windDirection, humidity);
+            weatherDataList.add(weatherData);
+        }
+
+        return weatherDataList;
+    }
+}
--- src/WeatherArtGenerator.java
+++ src/WeatherArtGenerator.java
@@ -26,10 +26,10 @@ public class WeatherArtGenerator {
     public static void main(String[] args) {
         try {
             // Read JSON data from the file
-            String jsonData = readJSONFromFile("input/data.txt");
+            String jsonData = JSONProcessor.readJSONFromFile("input/data.txt");
 
             // Parse the JSON data into a list of WeatherData objects
-            List<WeatherData> weatherDataList = parseJSONData(jsonData);
+            List<WeatherData> weatherDataList = JSONProcessor.parseJSONData(jsonData);
 
             List<Segment> windSegments = generateWindCurve(weatherDataList);
 
@@ -41,63 +41,6 @@ public class WeatherArtGenerator {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    /**
-     * Reads JSON data from a file and returns it as a string.
-     *
-     * @param fileName The name of the JSON file.
-     * @return The JSON data as a string.
-     * @throws IOException If an I/O error occurs while reading the file.
-     */
-    public static String readJSONFromFile(String fileName) throws IOException {
-        StringBuilder jsonStr = new StringBuilder();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                jsonStr.append(line);
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-        return jsonStr.toString();
-    }
-
-    /**
-     * Parses JSON data into a list of WeatherData objects.
-     *
-     * @param jsonData The JSON data as a string.
-     * @return A list of WeatherData objects.
-     */
-    public static List<WeatherData> parseJSONData(String jsonData) {
-        List<WeatherData> weatherDataList = new ArrayList<>();
-
-        // Parse the JSON data
-        JSONObject jsonObject = new JSONObject(jsonData);
-
-        // Extract the "days" array
-        JSONArray daysArray = jsonObject.getJSONArray("days");
-
-        // Loop through each day's data
-        for (int i = 0; i < daysArray.length(); i++) {
-            JSONObject dayObject = daysArray.getJSONObject(i);
-
-            // Extract weather parameters for each date
-            float temperature = dayObject.getFloat("temp");
-            float windSpeed = dayObject.getFloat("windspeed");
-            float humidity = dayObject.getFloat("humidity");
-            float windDirection = dayObject.getFloat("winddir");
-
-            // Create a WeatherData object for the date and add it to the list
-            WeatherData weatherData = new WeatherData(temperature, windSpeed,
-                    windDirection, humidity);
-            weatherDataList.add(weatherData);
-        }
-
-        return weatherDataList;
     }
 
     /**
@@ -265,7 +208,7 @@ public class WeatherArtGenerator {
     /**
      * Plots wind curves on a JPanel.
      *
-     * @param windSegments A list of wind curve segments.
+     * @param windSubegments A list of wind curve segments.
      */
     public static void createAndDisplayFrame(List<List<Segment>> windSubegments) throws IOException {
         // Create a BufferedImage to draw the wind curves on
